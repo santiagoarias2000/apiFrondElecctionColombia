b7412d60c5cb72088d25040750026ef47af43670
santiagoarias2000 <santiago.ariasm@usantoto.edu.co>
2022-12-26 14:23:53 -0500

    first comment to app

---
+++ src/app/util/domins/ApiBack.tsx
@@ -0,0 +1,8 @@
+const ApiBack = {
+    //URL connect to backend from application...
+    URL: "http://localhost:3123",
+    //Routes private to back and send go to information...
+    //Amazonas
+    AMAZONAS_PRIVATE_VIEW:"/api/private/amazonas/view",
+};
+export default ApiBack;
